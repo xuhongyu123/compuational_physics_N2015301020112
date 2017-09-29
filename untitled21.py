c7805e48a2ebdcc1fa06561b089c0f68eccb599f
xuhongyu123 <31839823+xuhongyu123@users.noreply.github.com>
2017-09-30 01:00:01 +0800

    Add files via upload

---
+++ untitled21.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 29 21:28:59 2017
+
+@author: pc
+"""
+import matplotlib.pyplot as plt
+def euler(t0,x0,dt,v):    
+    t,x = t0,x0
+    list_time=[t0,]
+    list_position=[x0,]
+    while t <= 10:
+        print("%f,%f" %(t,x))
+        t+=dt
+        x+=v*dt
+        list_time.append(t)
+        list_position.append(x)
+euler(0,0,0.05,40)
+plt.plot(list_time,list_position)
+plt.ylabel('position(m)')
+plt.xlabel('time(s)')
+plt.title('x-t')
+plt.ylim(0,400)
+plt.xlim(0,10)
+plt.show()
